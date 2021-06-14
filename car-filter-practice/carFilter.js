bca1660a68f6a620d422642092edcc1399c8d353
cegan1234 <connor.egan@gmail.com>
2021-06-14 17:16:29 -0500

    working test version

--- car-filter-practice/carFilter.js
+++ car-filter-practice/carFilter.js
@@ -1,4 +1,13 @@
 
-export function filterCars(cars, filters) {
-
+module.exports.filterCars = function filterCars(cars, countryOfOrigin) {
+    let carsThatMatchFilter = []
+    for (let i = 0; i < cars.length; i++) {
+        let car = cars[i]
+        if (car.countryOfOrigin === countryOfOrigin) {
+            carsThatMatchFilter.push(car)
+        }
+    }
+    return carsThatMatchFilter
 }
+
+
--- car-filter-practice/carFilterTest.js
+++ car-filter-practice/carFilterTest.js
@@ -1,8 +1,50 @@
-import { filterCars } from './carFilter'
+const chai = require('chai');
+const expect = chai.expect;
+
+const filterer = require('./carFilter')
+const filterCars = filterer.filterCars;
 
 describe('test filterCars', () => {
 
   it('returns empty array when empty array passed in', () => {
-    expect(filterCars([])).toBe([])
+    expect(filterCars([])).to.deep.equal([])
+  })
+
+  it('filters correct list of cars', () => {
+    const arrayOfCars = [
+      {
+        countryOfOrigin: "Germany",
+        model: "BMW",
+      },
+      {
+        countryOfOrigin: "Japan",
+        model: "Toyota"
+      }
+    ]
+    const expectedResult = [
+
+      {
+        countryOfOrigin: "Japan",
+        model: "Toyota"
+      }
+    ]
+     expect(filterCars(arrayOfCars,'Japan')).to.deep.equal(expectedResult)
+
+  })
+
+  it('returns an empty array if no cars match',()=> {
+    const arrayOfCars = [
+      {
+        countryOfOrigin: "Germany",
+        model: "BMW",
+      },
+      {
+        countryOfOrigin: "Japan",
+        model: "Toyota"
+      }
+    ]
+    const expectedResult = []
+    expect(filterCars(arrayOfCars,'USA')).to.deep.equal(expectedResult)
+
   })
 })
